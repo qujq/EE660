9d9a406d00feed6490937a02ee24b3cdda776f0d
qujq-ubuntu <qujq@outlook.com>
2022-10-23 18:37:24 -0700

    finish P1

--- Midterm/P1.py
+++ Midterm/P1.py
@@ -25,32 +25,55 @@ y_val = [y / y_train_mean for y in y_val]
 y_test = [y / y_train_mean for y in y_test]
 
 # no regularization
-# reg_no_regularization = LinearRegression(fit_intercept=False).fit(X_train, y_train)
-# y_pred_no_regularization = reg_no_regularization.predict(X_val)
-# mse_no_regularization = mean_squared_error(y_pred_no_regularization, y_val)
-# print(mse_no_regularization)
+reg_no_regularization = LinearRegression(fit_intercept=False).fit(X_train, y_train)
+y_pred_no_regularization = reg_no_regularization.predict(X_val)
+mse_no_regularization = mean_squared_error(y_pred_no_regularization, y_val)
+print(mse_no_regularization)
 
-# for log_lambda in range(-10, 11, 1):
-#     cur_lambda = 2 ** log_lambda
+y_test_pred_no_regularization = reg_no_regularization.predict(X_test)
+mse_test_no_regularization = mean_squared_error(y_test_pred_no_regularization, y_test)
+print(mse_test_no_regularization)
 
-#     reg_l1 = linear_model.Lasso(alpha=cur_lambda, fit_intercept=False)
-#     reg_l1.fit(X_train, y_train)
-#     y_pred_l1 = reg_l1.predict(X_val)
-#     mse_l1 = mean_squared_error(y_pred_l1, y_val)
+for log_lambda in range(-10, 11, 1):
+    cur_lambda = 2 ** log_lambda
 
-#     reg_l2 = linear_model.Ridge(alpha=cur_lambda, fit_intercept=False)
-#     reg_l2.fit(X_train, y_train)
-#     y_pred_l2 = reg_l2.predict(X_val)
-#     mse_l2 = mean_squared_error(y_pred_l2, y_val)
-#     print("lambda: ", cur_lambda,  " mse (l1): ", mse_l1, " mse (l2): ", mse_l2)
+    reg_l1 = linear_model.Lasso(alpha=cur_lambda, fit_intercept=False)
+    reg_l1.fit(X_train, y_train)
+    y_pred_l1 = reg_l1.predict(X_val)
+    mse_l1 = mean_squared_error(y_pred_l1, y_val)
+
+    # test
+    y_test_pred_l1 = reg_l1.predict(X_test)
+    mse_test_l1 = mean_squared_error(y_test_pred_l1, y_test)
+
+    print(mse_l1, mse_test_l1, reg_l1.coef_)
+
+    reg_l2 = linear_model.Ridge(alpha=cur_lambda, fit_intercept=False)
+    reg_l2.fit(X_train, y_train)
+    y_pred_l2 = reg_l2.predict(X_val)
+    mse_l2 = mean_squared_error(y_pred_l2, y_val)
+
+    # test
+    y_test_pred_l2 = reg_l2.predict(X_test)
+    mse_test_l2 = mean_squared_error(y_test_pred_l2, y_test)
+
+    print("lambda: ", cur_lambda,  " mse (l1): ", mse_l1, " mse (l2): ", mse_l2)
+    print("test lambda: ", cur_lambda,  " mse (l1): ", mse_test_l1, " mse (l2): ", mse_test_l2)
 
 min_mse = 10
 res_thod = 0
-for i in range(1, 10000, 100):
+for i in range(1, 101, 100):
     # reg_RANSAC = RANSACRegressor(random_state=0, min_samples=0.98, max_trials=100).fit(X_train, y_train)
     reg_RANSAC = RANSACRegressor(random_state=0, min_samples=0.98, max_trials=100, residual_threshold=0.23).fit(X_train, y_train)
     y_pred_RANSAC = reg_RANSAC.predict(X_val)
     mse_RANSAC = mean_squared_error(y_pred_RANSAC, y_val)
+
+    # test
+    y_test_pred_RANSAC = reg_RANSAC.predict(X_test)
+    mse_test_RANSAC = mean_squared_error(y_test_pred_RANSAC, y_test)
+
+    print(mse_RANSAC, mse_test_RANSAC)
+
     if mse_RANSAC < min_mse:
         min_mse = mse_RANSAC
         res_thod = i
