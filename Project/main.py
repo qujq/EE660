29767e6718d0d3e9de98d18b026ed69b342e2f03
qujq <11573770+qujq2022@user.noreply.gitee.com>
2022-11-25 19:02:49 -0800

    add subspace alignment

--- Project/main.py
+++ Project/main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn import tree
+import matplotlib.pyplot as plt
 
 data = np.array(pd.read_csv('./heart_disease_health_indicators_BRFSS2015.csv', header = 0, usecols = ['HeartDiseaseorAttack','HighBP','HighChol','CholCheck','BMI','Smoker','Stroke','Diabetes','PhysActivity','Fruits','Veggies','HvyAlcoholConsump','AnyHealthcare','NoDocbcCost', 'GenHlth', 'MentHlth','PhysHlth','DiffWalk', 'Age', 'Income']).values)
 
@@ -36,6 +38,9 @@ y_s = high_income[:, 0]
 
 X_t = low_income[:, 1:(len(high_income) - 1)]
 y_t = low_income[:, 0]
+
+print(len(X_s))
+print(len(X_t))
 
 def standardization_normalization(column_data, method="standardization"):
     if method == "standardization":
@@ -101,3 +106,78 @@ print(clf.score(X_t_test, y_t_test))
 clf = tree.DecisionTreeClassifier()
 clf.fit(X_s_train, y_s_train)
 print(clf.score(X_t_test, y_t_test))
+
+def get_sign_matrix(length):
+    end = 2 ** length - 1
+    start = 0
+    res = []
+    while start <= end:
+        cur = start
+        cur_bits = []
+        while cur > 0:
+            cur_bits.append(cur & 1)
+            cur = cur >> 1
+        while len(cur_bits) < length:
+            cur_bits.append(0)
+        for i in range(len(cur_bits)):
+            if cur_bits[i] == 0:
+                cur_bits[i] = -1
+        res.append(cur_bits)
+        start += 1
+    return res
+
+def subspace_alignment(Xs, Ys, Xt_test, yt_test, clf, X_t_train=None, y_t_train=None, num_features=19):
+    source_pca = PCA(num_features)
+    source_pca.fit(Xs)
+
+    target_pca = PCA(num_features)
+    target_pca.fit(Xt_test)
+
+    M = source_pca.components_ @ target_pca.components_.T
+    Xs_tf = source_pca.transform(Xs) @ M
+    clf.fit(Xs_tf, Ys)
+    
+    if X_t_train is None or y_t_train is None:
+        Xt_tf = target_pca.transform(Xt_test)
+        score = clf.score(Xt_tf, yt_test)
+    else:
+        sign_martix = None
+        bestacc = 0
+        for diag in get_sign_matrix(num_features):
+            cur_sign_martix = np.zeros((num_features, num_features), int)
+            np.fill_diagonal(cur_sign_martix, diag)
+
+            Xt_tf = target_pca.transform(X_t_train) @ cur_sign_martix
+            acc = clf.score(Xt_tf, y_t_train)
+            if acc > bestacc:
+                bestacc = acc
+                sign_martix = cur_sign_martix
+
+        Xt_test_tf = target_pca.transform(Xt_test) @ sign_martix
+        score = clf.score(Xt_test_tf, yt_test)
+    print(score)
+    return score
+
+plt.figure()
+clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+acc_list = []
+
+N = 11
+# for i in range(1, len(X_s_train[0])):
+for i in range(1, N):
+    print(i)
+    acc_list.append(subspace_alignment(X_s_train, y_s_train, X_t_test, y_t_test, clf, num_features=i))
+plt.plot(range(1, len(acc_list) + 1, 1), acc_list, label="SVM without target domain data")
+
+for N_tl in [10, 100, 1000, 2000, 3000, 4000]:
+    acc_list_ntl = []
+    for i in range(1, N):
+        X_tl = np.concatenate((X_t_train[:N_tl//2],X_t_train[-N_tl//2:]), axis=0)
+        y_tl = np.concatenate((y_t_train[:N_tl//2],y_t_train[-N_tl//2:]), axis=0)
+        acc_list_ntl.append(subspace_alignment(X_s_train, y_s_train, X_t_test, y_t_test, clf, X_t_train=X_tl, y_t_train=y_tl, num_features=i))
+    plt.plot(range(1, len(acc_list_ntl) + 1, 1), acc_list_ntl, label="SVM with " + str(N_tl) + " target domain data")
+
+plt.xlabel("num of target domain data included")
+plt.ylabel("accuracy")
+plt.legend()
+plt.show()
